12d4eb626720a855dcfa0048d01819c7206b660d
jakob young <jakyoung@byu.edu>
2024-11-12 22:12:51 -0700

    finished a test. it did not like the java.client package... broke it? hopefully that is fixable

---
+++ client/src/test/client/ServerFacadeTests.java
@@ -0,0 +1,39 @@
+package client;
+
+import org.junit.jupiter.api.*;
+import server.Server;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class ServerFacadeTests {
+
+    private static Server server;
+    private static ServerFacade facade;
+
+    @BeforeAll
+    public static void init() {
+        server = new Server();
+        var port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
+    }
+
+    @AfterAll
+    public static void stopServer() {
+        server.stop();
+    }
+
+    @BeforeEach
+    public void clearDatabase() throws Exception {
+        facade.clearDB();
+    }
+
+    @Test
+    void testRegisterSuccess() throws Exception {
+        var result = facade.register("testUser", "testPassword", "test@example.com");
+        assertNotNull(result);
+        assertNotNull(result.authToken());
+    }
+
+}
--- server/src/main/java/server/requests/ListGamesRequest.java
+++ server/src/main/java/server/requests/ListGamesRequest.java
@@ -3,4 +3,8 @@ package server.requests;
 public class ListGamesRequest {
     public ListGamesRequest(String authToken) {
     }
+
+    public ListGamesRequest() {
+
+    }
 }
